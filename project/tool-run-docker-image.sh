e88dfda9f31e7aab0262a8c116e012722e1fd275
Shaper2111 <Shaper2010@yandex.ru>
2018-01-04 13:24:53 +0300

    sync with latest templates

--- project/tool-run-docker-image.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-./tool-rm-gen-and-package.sh
-cp target/celerio-game-editor-admin.jar docker/.
-cd docker
-docker build -t celerio-game-editor-admin .
-docker run -p 8080:8080 celerio-game-editor-admin
